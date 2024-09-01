ce54f0f2a9ebaf06b6596688700adb9e25f94253
RendevMq <rensystem73@gmail.com>
2024-09-01 07:54:19 -0500

    update

--- src/main/java/com/rensystem/apirest/apirest/ApirestApplication.java
+++ src/main/java/com/rensystem/apirest/apirest/ApirestApplication.java
@@ -10,4 +10,13 @@ public class ApirestApplication {
 		SpringApplication.run(ApirestApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
