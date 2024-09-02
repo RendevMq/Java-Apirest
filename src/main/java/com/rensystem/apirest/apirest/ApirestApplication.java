58bf1344b52d8ae393d807ef0c2fb61e53024b47
RendevMq <rensystem73@gmail.com>
2024-09-02 02:47:34 -0500

    id in order LATEST

--- src/main/java/com/rensystem/apirest/apirest/ApirestApplication.java
+++ src/main/java/com/rensystem/apirest/apirest/ApirestApplication.java
@@ -1,6 +1,5 @@
 package com.rensystem.apirest.apirest;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -10,22 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class ApirestApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ApirestApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestApplication.class, args);
+	}
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        //String url = dotenv.get("SPRING_URL"); // Leer la variable del archivo .env
-
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://java-apirest-crud-railway.vercel.app")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
-    }
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://java-apirest-crud-railway.vercel.app").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
