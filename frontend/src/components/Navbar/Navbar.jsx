54feb51b3ccefc177ac8fce740b6e5bcaa903caf
RendevMq <rensystem73@gmail.com>
2024-09-02 02:52:17 -0500

    id in order LATEST

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import styles from "./Navbar.module.css";
-
+import { AiFillGithub } from "react-icons/ai";
 const Navbar = ({ theme, toggleTheme }) => {
   return (
     <nav className={styles.navbar}>
-      <div className={styles.logo}>RENDEVMQ</div>
+      <div className={styles.logo}>
+        RENDEVMQ
+        <Button
+          href="https://github.com/RendevMq"
+          target="_blank"
+          className="fork-btn-inner"
+        >
+          <AiFillGithub style={{ fontSize: "1.2em" }} />{" "}
+          {/* <AiFillStar style={{ fontSize: "1.1em" }} /> */}
+        </Button>
+      </div>
       <div className={styles.themeSwitch}>
         <input
           type="checkbox"
