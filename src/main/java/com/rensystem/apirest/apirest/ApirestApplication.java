308e8e418b9e94cf74f5968d3e0be3181a31288b
RendevMq <rensystem73@gmail.com>
2024-09-01 21:55:17 -0500

    front back

--- src/main/java/com/rensystem/apirest/apirest/ApirestApplication.java
+++ src/main/java/com/rensystem/apirest/apirest/ApirestApplication.java
@@ -2,6 +2,9 @@ package com.rensystem.apirest.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApirestApplication {
@@ -10,4 +13,13 @@ public class ApirestApplication {
 		SpringApplication.run(ApirestApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://java-apirest-crud-railway.vercel.app").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
