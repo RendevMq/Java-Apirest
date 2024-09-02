4593475ec10663086586caa9779c6968183e926d
RendevMq <rensystem73@gmail.com>
2024-09-02 02:12:10 -0500

    id in order LATEST

--- src/main/java/com/rensystem/apirest/apirest/ApirestApplication.java
+++ src/main/java/com/rensystem/apirest/apirest/ApirestApplication.java
@@ -1,5 +1,6 @@
 package com.rensystem.apirest.apirest;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,18 +10,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class ApirestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApirestApplication.class, args);
-	}
-	String url = dotenv.get("SPRING_URL");
+    public static void main(String[] args) {
+        SpringApplication.run(ApirestApplication.class, args);
+    }
 
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins(url).allowedMethods("*").allowedHeaders("*");
-			}
-		};
-	}
+    @Bean
+    public Dotenv dotenv() {
+        return Dotenv.load();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(Dotenv dotenv) {
+        String url = dotenv.get("SPRING_URL"); // Leer la variable del archivo .env
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(url)
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 }
